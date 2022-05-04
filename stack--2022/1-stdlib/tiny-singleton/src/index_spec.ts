fd7a3c81648d9a3516aed9634a6d00ca9bf76a1e
Offirmo <offirmo.net@gmail.com>
2022-05-04 21:28:16 +1000

    [chore] slightly improved unit tests

--- stack--2022/1-stdlib/tiny-singleton/src/index_spec.ts
+++ stack--2022/1-stdlib/tiny-singleton/src/index_spec.ts
@@ -19,15 +19,16 @@ describe('tiny-singleton', function () {
 	}
 
 	context('when the generator is taking no params', function () {
+
 		it('should work and prevent subsequent creations', () => {
 			const get_person: () => Person = tiny_singleton(() => create_person('Luke'))
 
 			expect(get_person()).to.equal(get_person())
 			expect(get_person().ask_name()).to.equal('Luke')
-			expect(get_person().ask_name()).to.equal('Luke')
 			expect(get_person()).to.equal(get_person())
+			expect(get_person().ask_name()).to.equal('Luke')
 
-			// this line should not compile
+			// this line should trigger a TypeScript error
 			// @ts-expect-error
 			get_person().foo
 		})
@@ -39,20 +40,9 @@ describe('tiny-singleton', function () {
 			const get_owner: (name?: string) => Person = tiny_singleton((name?: string) => create_person(name || 'Luke'))
 
 			expect(get_owner('Anakin').ask_name() === 'Anakin')
-			expect(get_owner('Luke').ask_name() === 'Anakin') // no change
+			expect(get_owner('Luke').ask_name() === 'Anakin') // no change, previous invocation got priority
 			expect(get_owner().ask_name() === 'Anakin')
-			expect(get_owner()).to.equal(get_owner())
+			expect(get_owner()).to.equal(get_owner()) // still stable
 		})
 	})
 })
-
-
-
-
-
-
-
-
-
-
-
