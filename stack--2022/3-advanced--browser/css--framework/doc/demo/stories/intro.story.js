718336cb97288e2c7bfeb7cb4f1fcbeb7a4199c0
Offirmo <offirmo.net@gmail.com>
2022-06-11 17:36:13 +1000

    +++

--- stack--2022/3-advanced--browser/css--framework/doc/demo/stories/intro.story.js
+++ stack--2022/3-advanced--browser/css--framework/doc/demo/stories/intro.story.js
@@ -3,7 +3,7 @@ export default {
 	title: 'Foo',
 	decorators: [
 		(Story) => {
-			import('@offirmo-private/css--sane-defaults')
+			import('@offirmo-private/css--foundation')
 			return Story
 		}
 	]
