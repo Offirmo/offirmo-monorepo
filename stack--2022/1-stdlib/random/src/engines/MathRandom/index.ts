58a587c42552b25a69a6fd1d113123c619ad72d7
Offirmo <offirmo.net@gmail.com>
2023-03-14 21:30:02 +1100

    +++

--- stack--2022/1-stdlib/random/src/engines/MathRandom/index.ts
+++ stack--2022/1-stdlib/random/src/engines/MathRandom/index.ts
@@ -1,5 +1,8 @@
 import { Int32, RNGEngine } from '../../types.js'
 
+// Math.random() is pretty good if no need for crypto
+// cf. https://v8.dev/blog/math-random
+
 export function get_RNGⵧMathᐧrandom(): RNGEngine {
 	const next_int32: Int32 = Math.random() * 0x100_000_000 | 0
 	return {
