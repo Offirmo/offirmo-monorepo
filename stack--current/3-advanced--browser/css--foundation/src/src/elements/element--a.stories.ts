8adcdbb98cbcd2d952d16fda47ca54b56a254687
Offirmo <offirmo.net@gmail.com>
2025-03-27 10:55:12 +1100

    +++

--- stack--current/3-advanced--browser/css--foundation/src/src/elements/element--a.stories.ts
+++ stack--current/3-advanced--browser/css--foundation/src/src/elements/element--a.stories.ts
@@ -1,1 +1,23 @@
 export * from '@offirmo-private/storypad/stories/html/elements/a'
+import { _link } from '@offirmo-private/storypad/stories/html/elements/a'
+
+export function CustomStyling1() {
+	return `
+<style>
+[data-o-theme="light-test"] { /* data-o-theme="light-test" */
+	color-scheme: light;
+
+	--o⋄color⁚fg--main: light-dark(#586E75, #839496);
+	--o⋄color⁚bg--main: light-dark(#FDF6E3, #002B36);
+
+	--o⋄color⁚fg--link: light-dark(#268BD2, #268BD2);
+	--o⋄color⁚fg--link--active: light-dark(#268BD2, #268BD2);
+	--o⋄color⁚fg--link--visited: light-dark(#268BD2, #268BD2);
+}
+</style>
+
+<div data-o-theme="light-test">
+	${_link()}
+</div>
+	`
+}
---
+++ stack--current/5-incubator/active/x-sandbox/src/1-active/library/view--book-stash--react/book/index.tsx
@@ -0,0 +1,33 @@
+/** Je développe un liseuse de livres électroniques. Écrit un component React affichant une rangée de livres comme sur une bibliothèque.
+*/
+
+import type { Immutable } from '@offirmo-private/ts-types'
+import renderⵧto_react from '@offirmo-private/rich-text-format--to-react'
+//import React, { use, useId, useEffect, useState } from 'react'
+
+import { type BookStash } from '../../model--book-stash'
+import { renderꓽbookshelf } from '../../view--book-stash--rich-text'
+
+/////////////////////////////////////////////////
+
+interface Props {
+	page_result: Immutable<BookStash>
+}
+function Component({ state }: Props) {
+	const $doc = renderꓽbookshelf(state)
+
+	return renderⵧto_react($doc)
+/*	return (
+		<div>
+			Hello, world!
+		</div>
+	)*/
+}
+
+/////////////////////////////////////////////////
+
+export {
+	type Props,
+	Component as xxx,
+}
+export default Component
