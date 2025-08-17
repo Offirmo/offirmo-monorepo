fac522279b6972da85e6745e36beb6fec05d9ecf
Offirmo <offirmo.net@gmail.com>
2025-08-18 08:17:24 +1000

    TLDW :scream_cat:

--- stack--current/C-final/@infinite-monorepo/70-operation--apply/module/~~sandbox/index.ts
+++ stack--current/C-final/@infinite-monorepo/70-operation--apply/module/~~sandbox/index.ts
@@ -2,6 +2,8 @@
 import { loadꓽspec } from '@infinite-monorepo/load-spec'
 import * as StateLib from '@infinite-monorepo/state'
 import { dumpꓽanyⵧprettified } from '@offirmo-private/prettify-any'
+import pluginꓽnvm from '@infinite-monorepo/plugin--nvm'
+import type { Plugin } from '@infinite-monorepo/types'
 
 
 /////////////////////////////////////////////////
@@ -10,13 +12,29 @@ import { dumpꓽanyⵧprettified } from '@offirmo-private/prettify-any'
 async function apply() {
 	console.log(`@infinite-monorepo/apply…`)
 
+	////////////
 	let state = StateLib.create()
 
 	const spec = await loadꓽspec()
 	state = StateLib.onꓽspec_loaded(state, spec)
 
+	dumpꓽanyⵧprettified('state', state)
+
+	////////////
+	const plugins: Array<Plugin> = [
+		pluginꓽnvm
+	]
+	plugins.forEach(plugin => {
+		state = plugin.onꓽload(state)
+	})
+
 	// TODO all root files
 	// TODO nvmrc
+
+	////////////
+	plugins.forEach(plugin => {
+		state = plugin.onꓽapply(state)
+	})
 
 	dumpꓽanyⵧprettified('state', state)
 
