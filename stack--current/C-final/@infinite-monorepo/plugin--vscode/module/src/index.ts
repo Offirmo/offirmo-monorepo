369ca9815d99b1438074f4b6f18bb3ee8ee3c63c
Offirmo <offirmo.net@gmail.com>
2025-11-23 18:54:35 +1100

    +++

---
+++ stack--current/C-final/@infinite-monorepo/plugin--vscode/module/src/index.ts
@@ -0,0 +1,9 @@
+
+import { manifestꓽᐧgitignore } from '@infinite-monorepo/plugin--git'
+
+
+A file called .vscode/settings.json will be created in your workspace with the following configuration:
+
+{
+	"deno.enable": true
+}
--- stack--web3/.monorepo/index.ts
+++ stack--web3/.monorepo/index.ts
@@ -5,7 +5,7 @@ import type { MonorepoSpec } from '@infinite-monorepo/types'
 const SPEC: Partial<MonorepoSpec> = {
 	namespace: '@offirmo',
 
-	runtimeⵧlocal: { name: 'node', versionsⵧacceptable: '^24' }, // imminent LTS
+	runtimeⵧlocal: { name: 'deno', versionsⵧacceptable: '^24' }, // imminent LTS
 	package_manager: 'pnpm', // 2025/10 we need some unique security features
 
 	workspaces: [
