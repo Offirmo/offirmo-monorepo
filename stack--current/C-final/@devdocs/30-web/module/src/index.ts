0394b6bf3d922e1cf6a6d2d3eecd45e591dc851e
Offirmo <offirmo.net@gmail.com>
2026-02-08 22:41:29 +1100

    +++

--- stack--current/C-final/@devdocs/30-web/module/src/index.ts
+++ stack--current/C-final/@devdocs/30-web/module/src/index.ts
@@ -7,7 +7,10 @@ import assert from 'tiny-invariant'
 /////////////////////////////////////////////////
 
 assert(!!globalThis.SharedWorker, 'SharedWorker should be available')
-const workerⵧshared = new SharedWorker(new URL('./worker--shared.ts', import.meta.url))
+const workerⵧshared = new SharedWorker(
+	new URL('./worker--shared.ts', import.meta.url),
+	{type: 'module'}
+)
 workerⵧshared.port.onmessage = (e) => {
 	console.log("Message received from worker--shared", e);
 };
@@ -18,9 +21,19 @@ workerⵧshared.port.postMessage('ping');
 
 assert(!!navigator.serviceWorker, 'SharedWorker should be available')
 
-navigator.serviceWorker.register(new URL('./worker--service.ts', import.meta.url))
+navigator.serviceWorker.register(
+	new URL('./worker--service.ts', import.meta.url),
+	{type: 'module', scope: "/",}
+)
 	.then(registration => {
 		console.log("Service worker registration succeeded:", registration)
+		if (registration.installing) {
+			console.log("Service worker -> installing");
+		} else if (registration.waiting) {
+			console.log("Service worker -> installed");
+		} else if (registration.active) {
+			console.log("Service worker -> active");
+		}
 	})
 	.catch(err => {
 		console.error(`Service worker registration failed: ${err}`)
--- stack--current/C-final/@devdocs/30-web/module/src/worker--service.ts
+++ stack--current/C-final/@devdocs/30-web/module/src/worker--service.ts
@@ -8,24 +8,34 @@ console.log(`[workerⵧservice] Hello!`)
 self.addEventListener("install", (event) => {
 	console.log(`[workerⵧservice] ϟinstall`, event)
 
-	event.waitUntil(async () => {
+	console.log('parcel stuff:', { manifest, version })
+	/*event.waitUntil(async () => {
 		const cache = await caches.open(version);
 		await cache.addAll(manifest);
-	})
+	})*/
 })
 
 self.addEventListener("activate", (event) => {
 	console.log(`[workerⵧservice] ϟactivate`, event)
 
-	event.waitUntil(async () => {
+	event.waitUntil(clients.claim());
+	/*event.waitUntil(async () => {
 		const keys = await caches.keys();
 		await Promise.all(
 			keys.map(key => key !== version && caches.delete(key))
 		);
-	})
+	})*/
 })
 
 self.addEventListener("fetch", (event) => {
-	console.log(`[workerⵧservice] ϟfetch`, event)
-	throw new Error('Not implemented!')
+	console.log(`[workerⵧservice] ϟfetch ${event?.request?.url}`, event)
+
+	event.respondWith((async () => {
+		if (event.request.destination === 'iframe') {
+			console.warn(`[workerⵧservice] hijacking!`)
+			return new Response('TODO generate from SW')
+		}
+
+		return fetch(event.request.clone())
+	})())
 })
