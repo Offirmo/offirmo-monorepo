47c4376c0c71c89eebe4dd9267c13999288b79a0
Offirmo <offirmo.net@gmail.com>
2026-02-08 22:06:28 +1100

    +++

--- stack--current/C-final/@devdocs/30-web/module/src/worker--service.ts
+++ stack--current/C-final/@devdocs/30-web/module/src/worker--service.ts
@@ -1,11 +1,28 @@
+// https://web.dev/articles/offline-cookbook
+
+import {manifest, version} from '@parcel/service-worker';
+
+
 console.log(`[workerⵧservice] Hello!`)
 
 self.addEventListener("install", (event) => {
 	console.log(`[workerⵧservice] ϟinstall`, event)
+
+	event.waitUntil(async () => {
+		const cache = await caches.open(version);
+		await cache.addAll(manifest);
+	})
 })
 
 self.addEventListener("activate", (event) => {
 	console.log(`[workerⵧservice] ϟactivate`, event)
+
+	event.waitUntil(async () => {
+		const keys = await caches.keys();
+		await Promise.all(
+			keys.map(key => key !== version && caches.delete(key))
+		);
+	})
 })
 
 self.addEventListener("fetch", (event) => {
